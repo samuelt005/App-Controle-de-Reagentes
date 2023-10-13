bf1d3e9b4b2a4a5182adb6ffa7bbd11e63af85a3
Samuel <samuel.thomas1109@outlook.com>
2023-10-13 17:31:39 -0300

    Página de Login Estática

---
+++ App/src/app/pages/login/login.component.ts
@@ -0,0 +1,26 @@
+import { AuthService } from './../../auth/auth.service';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  form: FormGroup = this.fb.group({
+    username: ['', Validators.required], //TODO add more validatos
+    password: ['', Validators.required], //TODO add more validatos
+  });
+
+  constructor(private AuthService: AuthService, private fb: FormBuilder) {}
+
+  login() {
+    let user = this.AuthService.login(
+      this.form.value.username,
+      this.form.value.password
+    );
+    if (!user) {
+      alert('Usuário ou senha inválido!');
+    }
+  }
+}
