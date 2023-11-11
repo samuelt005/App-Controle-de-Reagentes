b78df5ea3fa9b3982b567b57a9fe9c81d831c916
Samuel <samuel.thomas1109@outlook.com>
2023-11-11 17:42:19 -0300

    Base da Página de Relatórios

--- App/src/app/pages/reports/reports.component.ts
+++ App/src/app/pages/reports/reports.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { PageTitle } from 'src/app/interfaces';
 import { PageComponent } from 'src/app/shared';
 
 @Component({
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.scss'],
 })
-export class ReportsComponent extends PageComponent {}
+export class ReportsComponent extends PageComponent {
+  pageTitle: PageTitle = {
+    iconColor: 'var(--secundaria-2)',
+    icon: 'description',
+    title: 'Gerar Relatórios',
+    searchBox: false,
+    adjustButton: false,
+  };
+}
